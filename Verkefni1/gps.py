abacf0fcc6eea2597b3834ad92a6de61ca99d8ca
Ingólfur Ari Jóhannsson <ingoar97@gmail.com>
2022-11-21 14:46:32 +0000

    fylkin orðin rétt

--- Verkefni1/gps.py
+++ Verkefni1/gps.py
@@ -15,16 +15,16 @@ def fall(x):
                      f4(x[0],x[1],x[2],x[3])])
 
 def f1(x,y,z,d):
-    return pow((x - system[0][0]),2) + pow((y - system[1][0]),2) + pow((z - system[2][0]),2) - pow(pow(c,2)*(system[3][0]-d),2)
+    return pow((x - system[0][0]),2) + pow((y - system[0][1]),2) + pow((z - system[0][2]),2) - pow(c,2)*pow((system[0][3]-d),2)
 
 def f2(x, y, z, d):
-    return pow((x - system[0][1]),2) + pow((y - system[1][1]),2) + pow((z - system[2][1]),2) - pow(pow(c,2)*(system[3][1]-d),2)
+    return pow((x - system[1][0]),2) + pow((y - system[1][1]),2) + pow((z - system[1][2]),2) - pow(c,2)*pow((system[1][3]-d),2)
 
 def f3(x, y, z, d):
-    return pow((x - system[0][2]),2) + pow((y - system[1][2]),2) + pow((z - system[2][2]),2) - pow(pow(c,2)*(system[3][2]-d),2)
+    return pow((x - system[2][0]),2) + pow((y - system[2][1]),2) + pow((z - system[2][2]),2) - pow(c,2)*pow((system[2][3]-d),2)
 
 def f4(x, y, z, d):
-    return pow((x - system[0][3]),2) + pow((y - system[1][3]),2) + pow((z - system[2][3]),2) - pow(pow(c,2)*(system[3][3]-d),2)
+    return pow((x - system[3][0]),2) + pow((y - system[3][1]),2) + pow((z - system[3][2]),2) - pow(c,2)*pow((system[3][3]-d),2)
 
 
 def dF(vigur):
@@ -45,15 +45,13 @@ def newtonmult(x0, tol):
         s = -la.solve(dF(x), fall(x))
         x = x + s
         counter +=1
-        if counter >= 2:
-            print("reiknaði of lengi")
+        if counter >=10 :
+            print("------------reiknaði of lengi------------")
             break
     return (x)
 
 if __name__ == '__main__':
 
     x0 = vigur
-    #x0 = np.transpose(x0)
-    print(system[1][2])
     tolerance = 0.01
     print(newtonmult(x0, tolerance))
