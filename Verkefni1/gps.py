f1c99eb02edcfc9aaf3682821946de604b4d20e2
Arnþór G <arnthorgisla@gmail.com>
2022-11-22 13:53:51 +0000

    laga klasann

--- Verkefni1/gps.py
+++ Verkefni1/gps.py
@@ -24,23 +24,23 @@ class newton:
                          self.f4(x[0],x[1],x[2],x[3])])
 
     def f1(self, x,y,z,d):
-        return pow((x - system[0][0]),2) + pow((y - system[0][1]),2) + pow((z - system[0][2]),2) - pow(c,2)*pow((system[0][3]-d),2)
+        return pow((x - self.system[0][0]),2) + pow((y - self.system[0][1]),2) + pow((z - self.system[0][2]),2) - pow(c,2)*pow((self.system[0][3]-d),2)
 
     def f2(self, x, y, z, d):
-        return pow((x - system[1][0]),2) + pow((y - system[1][1]),2) + pow((z - system[1][2]),2) - pow(c,2)*pow((system[1][3]-d),2)
+        return pow((x - self.system[1][0]),2) + pow((y - self.system[1][1]),2) + pow((z - self.system[1][2]),2) - pow(c,2)*pow((self.system[1][3]-d),2)
 
     def f3(self, x, y, z, d):
-        return pow((x - system[2][0]),2) + pow((y - system[2][1]),2) + pow((z - system[2][2]),2) - pow(c,2)*pow((system[2][3]-d),2)
+        return pow((x - self.system[2][0]),2) + pow((y - self.system[2][1]),2) + pow((z - self.system[2][2]),2) - pow(c,2)*pow((self.system[2][3]-d),2)
 
     def f4(self,x, y, z, d):
-        return pow((x - system[3][0]),2) + pow((y - system[3][1]),2) + pow((z - system[3][2]),2) - pow(c,2)*pow((system[3][3]-d),2)
+        return pow((x - self.system[3][0]),2) + pow((y - self.system[3][1]),2) + pow((z - self.system[3][2]),2) - pow(c,2)*pow((self.system[3][3]-d),2)
 
 
     def dF(self,vigur):
-        return np.array([[2*vigur[0]-2*system[0][0], 2*vigur[1]-2*system[0][1], 2*vigur[2]-2*system[0][2], 2*system[0][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
-                         [2*vigur[0]-2*system[1][0], 2*vigur[1]-2*system[1][1], 2*vigur[2]-2*system[1][2], 2*system[1][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
-                         [2*vigur[0]-2*system[2][0], 2*vigur[1]-2*system[2][1], 2*vigur[2]-2*system[2][2], 2*system[2][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
-                         [2*vigur[0]-2*system[3][0], 2*vigur[1]-2*system[3][1], 2*vigur[2]-2*system[3][2], 2*system[3][3]*pow(c,2)- 2*pow(c,2)*vigur[3]]])
+        return np.array([[2*vigur[0]-2*self.system[0][0], 2*vigur[1]-2*self.system[0][1], 2*vigur[2]-2*self.system[0][2], 2*self.system[0][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
+                         [2*vigur[0]-2*self.system[1][0], 2*vigur[1]-2*self.system[1][1], 2*vigur[2]-2*self.system[1][2], 2*self.system[1][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
+                         [2*vigur[0]-2*self.system[2][0], 2*vigur[1]-2*self.system[2][1], 2*vigur[2]-2*self.system[2][2], 2*self.system[2][3]*pow(c,2)- 2*pow(c,2)*vigur[3]],
+                         [2*vigur[0]-2*self.system[3][0], 2*vigur[1]-2*self.system[3][1], 2*vigur[2]-2*self.system[3][2], 2*self.system[3][3]*pow(c,2)- 2*pow(c,2)*vigur[3]]])
 
     def newtonmult(self, x0, tol):
         '''x0 er vigur i R^n skilgreindur t.d. sem
@@ -163,6 +163,8 @@ if __name__ == '__main__':
 
     plot3d(new_system)
 
+    n2 = newton(new_system_plus_skekkja)
+    print(n2.newtonmult(upphafsgildi, tolerance))
     # 4
     if False:
         for i in range(16):
