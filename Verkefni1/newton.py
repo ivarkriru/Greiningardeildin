84fca4bbbfd3f368c28c66e884bc00fe42b4dbe5
Ivar <ivar13@ru.is>
2022-11-23 15:44:27 +0000

    Newton gamlafall komið inn aftur

--- Verkefni1/newton.py
+++ Verkefni1/newton.py
@@ -50,3 +50,18 @@ class Newton:
                 print("------------reiknaði of lengi------------")
                 break
         return x
+    def NewtonOriginal(self, x0, tol):
+        '''x0 er vigur i R^n skilgreindur t.d. sem x0=np.array([1,2,3])
+        gert ráð fyrir að F(x) og Jacobi fylki DF(x) séu skilgreind annars staðar'''
+        x = x0
+        oldx = x + 2 * tol
+        counter = 0
+        while la.norm(x - oldx, np.inf) > tol:
+            oldx = x
+            s = -la.solve(self.dF(x), self.fall(x))
+            x = x + s
+            counter += 1
+            if counter >= 10:
+                print("------------reiknaði of lengi------------")
+                break
+        return (x)
