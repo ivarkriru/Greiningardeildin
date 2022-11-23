6561442e9f2ae9ea312ec13583aca2d669c0d9b4
Ingólfur Ari Jóhannsson <ingoar97@gmail.com>
2022-11-23 12:01:26 +0000

    bætti við föllum fyrir spurningum og hreynsað allskonar og fært

--- Verkefni1/gps.py
+++ Verkefni1/gps.py
@@ -1,21 +1,26 @@
 import random
-
 import numpy as np
 import math
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-import random
 
 system = np.array([[15600, 7540, 20140, 0.07074],
                    [18760, 2750, 18610, 0.07220],
                    [17610, 14630, 13480, 0.07690],
                    [19170, 610, 18390, 0.07242]])
+
+new_sat_pos = np.array([(np.pi / 8, -np.pi / 4),  # φ, θ, phi, theta
+                        (np.pi / 6, np.pi / 2),
+                        (3 * np.pi / 8, 2 * np.pi / 3),
+                        (np.pi / 4, np.pi / 6),
+                        ])
 c = 299792.458
 constaltitude = 26570
 earthaltitude = 6370
 tolerance = 0.01
 x0 = np.array([0, 0, 6370, 0])
 sat_teljari = 0
+skekkja = 1e-8
 
 class newton:
     def __init__(self, system=system):
@@ -95,7 +100,9 @@ def coords(phi, theta, altitude=constaltitude + earthaltitude):
     else:
         return "incorrect values"
 
-def plot3d(system):
+new_system = np.array([coords(*sat)[:-1] for sat in new_sat_pos])
+
+def plot3d(sys):
     fig = plt.figure()
 
     # syntax for 3-D projection
@@ -113,7 +120,7 @@ def plot3d(system):
         zhnit.append(svar[2])
 
     # plotting
-    n = newton(system)
+    n = newton(sys)
     ax.scatter(xhnit, yhnit, zhnit, c='blue', alpha=0.3)
     tolerance = 0.01
     svar = n.GaussNewton(x0, tolerance)
@@ -122,37 +129,12 @@ def plot3d(system):
     ax.scatter(x0[0], x0[1], x0[2], c='yellow', s=300)
     ax.set_title('3D line plot geeks for geeks')
 
-    for x in system:
+    for x in sys:
         ax.scatter(x[0], x[1], x[2], c='yellow', s=300)
 
     ax.set_proj_type('ortho')
     ax.set_box_aspect((1, 1, 1))
     plt.show()
-
-def sp3skekkja():
-    i1rettstutt = coords(math.pi / 8, -math.pi / 4)[0:4]
-    i2rettstutt = coords(math.pi / 6, math.pi / 2)[0:4]
-    i3rettstutt = coords(3 * math.pi / 8, 2 * math.pi / 3)[0:4]
-    i4rettstutt = coords(math.pi / 4, math.pi / 6)[0:4]
-
-    an_skekkju_system = np.array([i1rettstutt, i2rettstutt, i3rettstutt, i4rettstutt])
-
-    n = newton(an_skekkju_system)
-
-    svaranskekkju = n.GaussNewton(x0, tolerance)
-
-    print("Svar án skekkju : " + str(svaranskekkju))
-
-    i1rangt = coords( (math.pi / 8) + 0.000000001,-math.pi / 4)[0:4]
-    i2rangt = coords((math.pi / 6) + 0.000000001,math.pi / 2 )[0:4]
-    i3rangt = coords((3 * math.pi / 8) + 0.000000001,2 * math.pi / 3)[0:4]
-    i4rangt = coords((math.pi / 4) + 0.000000001,math.pi / 6 )[0:4]
-
-    skekkju_system = np.array([i1rangt, i2rangt, i3rangt, i4rangt])
-    n = newton(skekkju_system)
-    svarmedskekkju = n.GaussNewton(x0, tolerance)
-
-    print("Svar með skekkju : " + str(svarmedskekkju))
 
 def nyttSatPos():
     nytt_loc = coords(math.pi*random.random(), random.random()*10000, constaltitude)
@@ -161,29 +143,23 @@ def nyttSatPos():
     print("Gervihnöttur númer " + str(sat_teljari) + " : " + str(nytt_loc))
     return nytt_loc
 
-new_sat_pos = np.array([(np.pi / 8, -np.pi / 4),  # φ, θ, phi, theta
-                        (np.pi / 6, np.pi / 2),
-                        (3 * np.pi / 8, 2 * np.pi / 3),
-                        (np.pi / 4, np.pi / 6),
-                        ])
-
-if __name__ == '__main__':
-
+def spurning1():
     n = newton(system)
     svar = n.GaussNewton(x0, tolerance)
     print("---- svar 1 ----- :")
     print("X: " + '%.6f' % svar[0] + " Y: " + '%.6f' % svar[1] + " Z: " + '%.6f' % svar[2] + " d: " + '%.6f' % svar[3])
 
+def spurning2():
     svar_coords = coords(0, 0)
     print("---- svar 2 ----- :")
     print(f"A: {svar_coords[0]:.02f}, B: {svar_coords[1]:.02f}, C: {svar_coords[2]:.02f}, t: {svar_coords[3]:.02f}, d: {svar_coords[4]:.02f}")
 
-
+def spurning3():
     print("---- svar 3 ----- :")
-    new_system = np.array([coords(*sat)[:-1] for sat in new_sat_pos])
-    skekkja = 1e-8
 
-    new_system_plus_skekkja = np.array([coords(sat[0] + skekkja, sat[1])[:-1] if index < 2 else coords(sat[0] - skekkja, sat[1])[:-1] for index, sat in enumerate(new_sat_pos)])
+    new_system_plus_skekkja = np.array(
+        [coords(sat[0] + skekkja, sat[1])[:-1] if index < 2 else coords(sat[0] - skekkja, sat[1])[:-1] for index, sat in
+         enumerate(new_sat_pos)])
 
     # setja réttan tíma á skekkjukerfið
     for index, sat_pos in enumerate(new_system):
@@ -191,24 +167,25 @@ if __name__ == '__main__':
 
     n3 = newton(new_system)
     svaran = n3.GaussNewton(x0, tolerance)
-    print("lausnin án skekkju   X: " + '%.6f' % svaran[0] + " Y: " + '%.6f' % svaran[1] + " Z: " + '%.6f' % svaran[2] + " d: " + '%.6f' % svaran[3])
-
-    #sp3skekkja()
-    plot3d(new_system)
+    print("lausnin án skekkju   X: " + '%.6f' % svaran[0] + " Y: " + '%.6f' % svaran[1] + " Z: " + '%.6f' % svaran[
+        2] + " d: " + '%.6f' % svaran[3])
 
     n2 = newton(new_system_plus_skekkja)
     svarmed = n2.GaussNewton(x0, tolerance)
-    print("lausnin með skekkju  X: " + '%.6f' % svarmed[0] + " Y: " + '%.6f' % svarmed[1] + " Z: " + '%.6f' % svarmed[2] + " d: " + '%.6f' % svarmed[3])
+    print("lausnin með skekkju  X: " + '%.6f' % svarmed[0] + " Y: " + '%.6f' % svarmed[1] + " Z: " + '%.6f' % svarmed[
+        2] + " d: " + '%.6f' % svarmed[3])
     print("Skekkjan sjálf : " + '%.6f' % point_diff(svaran, svarmed) + " metrar")
-    # 4
 
+def spurning4():
     print("---- svar 4 ----- :")
     skekkja = 1e-8
     upphafsgildi = np.array([0, 0, 6370, 0])
     if True:
         list_of_positions = []
         for i in range(16):
-            new_system_with_error = np.array([coords(sat[0] + skekkja, sat[1])[:-1] if i & (1<<index)  else coords(sat[0] - skekkja, sat[1])[:-1] for index, sat in enumerate(new_sat_pos)])
+            new_system_with_error = np.array(
+                [coords(sat[0] + skekkja, sat[1])[:-1] if i & (1 << index) else coords(sat[0] - skekkja, sat[1])[:-1]
+                 for index, sat in enumerate(new_sat_pos)])
             # uncomment to verify if correct:
             # new_systems_with_error = np.array([1 if (i & (1<<index))  else 0 for index, sat in enumerate(new_sat_pos)])
             # print(new_systems_with_error)
@@ -224,28 +201,50 @@ if __name__ == '__main__':
     # plotta upp
     plt.scatter([i for i in range(16)], [point_diff(x0, position) for position in list_of_positions])
 
-    print(f"max: {max([point_diff(x0, position) for position in list_of_positions])*1000:.04f}m")
-    print(f"min: {min([point_diff(x0, position) for position in list_of_positions])*1000:.04f}m")
+    print(f"max: {max([point_diff(x0, position) for position in list_of_positions]) * 1000:.04f}m")
+    print(f"min: {min([point_diff(x0, position) for position in list_of_positions]) * 1000:.04f}m")
 
     A_dreifing = [position[0] for position in list_of_positions]
     B_dreifing = [position[1] for position in list_of_positions]
-    C_dreifing = [position[2]-earthaltitude for position in list_of_positions]
-    #plt.plot(A_dreifing)
-    #plt.plot(B_dreifing)
-    #plt.plot(C_dreifing)
-    #plt.show()
+    C_dreifing = [position[2] - earthaltitude for position in list_of_positions]
+    # plt.plot(A_dreifing)
+    # plt.plot(B_dreifing)
+    # plt.plot(C_dreifing)
+    # plt.show()
 
+def spurning5():
     # ------------- 5 ---------------
 
     new_sat_pos = np.array([[np.pi / 2, np.pi / 2],  # φ, θ, phi, theta
                             [np.pi / 2, np.pi / 2],
                             [np.pi / 2, np.pi / 2],
                             [np.pi / 2, np.pi / 2],
-                        ])
+                            ])
     skekkja5 = 1e-5
     for i in range(4):
-        new_sat_pos[i][0] + (random.randrange(-1, 1)*skekkja5)
-        new_sat_pos[i][1] + (random.randrange(-1, 1)*skekkja5)
+        new_sat_pos[i][0] + (random.randrange(-1, 1) * skekkja5)
+        new_sat_pos[i][1] + (random.randrange(-1, 1) * skekkja5)
     n5 = newton()
+def spurning6():
+    pass
+def spurning7():
+    pass
+def spurning8():
+    pass
+def spurning9():
+    pass
+
+if __name__ == '__main__':
+    spurning1()
+    spurning2()
+    spurning3()
+    spurning4()
+    spurning5()
+    spurning6()
+    spurning7()
+    spurning8()
+    spurning9()
+
+    plot3d(new_system)
 
 
