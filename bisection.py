74bdfd6787df558072d61b84d8d7763e638237fe
Gunnar Sigvaldason <gunnars22@ru.is>
2022-11-24 15:49:09 +0000

    prufa

---
+++ bisection.py
@@ -0,0 +1,22 @@
+def bisection(f,a,b,tol):
+    '''gert ráð fyrir að búið se að skilgreina f(x) fyrir utan t.d.
+    def f(x):
+        return(x**2-2)
+    '''
+    if f(a)*f(b) >= 0:
+        print("Bisection method fails.")
+        return None
+    else:
+        fa=f(a)
+        while (b-a)/2>tol:
+            c=(a+b)/2
+            fc=f(c)
+            if fc==0:break
+            if fc*fa<0:
+                b=c
+            else:
+                a=c
+                fa=fc
+    return((a+b)/2)
+
+
