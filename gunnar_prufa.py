71df38bdd302c418690aec2d79fcf328e78cf517
Gunnar Sigvaldason <gunnars22@ru.is>
2022-11-24 15:49:17 +0000

    prufa

--- gunnar_prufa.py
+++ gunnar_prufa.py
@@ -1,39 +1,45 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mayavi import mlab
+from tvtk.api import tvtk
+import matplotlib.pyplot as plt # only for manipulating the input image
 
-def mpl_sphere(image_file):
-    img = plt.imread(image_file)
+def manual_sphere(image_file):
+    # caveat 1: flip the input image along its first axis
+    img = plt.imread(image_file) # shape (N,M,3), flip along first dim
+    outfile = image_file.replace('.jpg', '_flipped.jpg')
+    # flip output along first dim to get right chirality of the mapping
+    img = img[::-1,...]
+    plt.imsave(outfile, img)
+    image_file = outfile  # work with the flipped file from now on
 
-    # define a grid matching the map size, subsample along with pixels
-    theta = np.linspace(0, np.pi, img.shape[0])
-    phi = np.linspace(0, 2*np.pi, img.shape[1])
+    # parameters for the sphere
+    R = 1 # radius of the sphere
+    Nrad = 180 # points along theta and phi
+    phi = np.linspace(0, 2 * np.pi, Nrad)  # shape (Nrad,)
+    theta = np.linspace(0, np.pi, Nrad)    # shape (Nrad,)
+    phigrid,thetagrid = np.meshgrid(phi, theta) # shapes (Nrad, Nrad)
 
-    count = 70 # keep 180 points along theta and phi
-    theta_inds = np.linspace(0, img.shape[0] - 1, count).round().astype(int)
-    phi_inds = np.linspace(0, img.shape[1] - 1, count).round().astype(int)
-    theta = theta[theta_inds]
-    phi = phi[phi_inds]
-    img = img[np.ix_(theta_inds, phi_inds)]
+    # compute actual points on the sphere
+    x = R * np.sin(thetagrid) * np.cos(phigrid)
+    y = R * np.sin(thetagrid) * np.sin(phigrid)
+    z = R * np.cos(thetagrid)
 
-    theta,phi = np.meshgrid(theta, phi)
-    r = 1
+    # create figure
+    mlab.figure(size=(600, 600))
 
-    # sphere
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
+    # create meshed sphere
+    mesh = mlab.mesh(x,y,z)
+    mesh.actor.actor.mapper.scalar_visibility = False
+    mesh.actor.enable_texture = True  # probably redundant assigning the texture later
 
-    # create 3d Axes
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(x.T, y.T, z.T, facecolors=img/255, cstride=1, rstride=1) # we've already pruned ourselves
+    # load the (flipped) image for texturing
+    img = tvtk.JPEGReader(file_name=image_file)
+    texture = tvtk.Texture(input_connection=img.output_port, interpolate=0, repeat=0)
+    mesh.actor.actor.texture = texture
 
-    # make the plot more spherical
-    ax.axis('scaled')
-
-
-if __name__ == "__main__":
-    image_file = 'blue_marble2.jpg'
-    mpl_sphere(image_file)
-    plt.show()
+    # tell mayavi that the mapping from points to pixels happens via a sphere
+    mesh.actor.tcoord_generator_mode = 'sphere' # map is already given for a spherical mapping
+    cylinder_mapper = mesh.actor.tcoord_generator
+    # caveat 2: if prevent_seam is 1 (default), half the image is used to map half the sphere
+    cylinder_mapper.prevent_seam = 0 # use 360 degrees, might cause seam but no fake data
+    #cylinder_mapper.center = np.array([0,0,0])  # set non-trivial center for the mapping sphere if necessary
