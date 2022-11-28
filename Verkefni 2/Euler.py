755daf34e341b38ed5d8d9936ae25179fded1082
Ivar <ivar13@ru.is>
2022-11-28 10:26:24 +0000

    Euler file

--- Verkefni 2/Euler.py
+++ Verkefni 2/Euler.py
@@ -1,6 +1,29 @@
 import numpy as np
-y = lambda t, y : t * y - np.power(y, 3)
+import matplotlib.pyplot as plt
+f = lambda t, yinner: np.power(t, 2) +2
 
 def euler(y0,n,T):
     h = T/n
     t = 0
+    xaxis = []
+    yaxis = []
+    xaxis.append(t)
+    yaxis.append(y0)
+    y=y0
+    for i in range(1, n):
+        t = t + h
+        xaxis.append(t)
+        yaxis.append(yaxis[i-1] + f(t,yaxis[i-1]) + h)
+
+    x = range(y0,T)
+    y=f(x,x)
+    plt.plot(x,y)
+    plt.plot(xaxis,yaxis)
+    np.linspace(0,5)
+    plt.show()
+
+
+euler(0,5,3)
+
+euler(0,20,3)
+euler(0,200,3)
