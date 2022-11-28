e3e0e67b0028a52eed67f76c25838350abcebbfe
Ivar <ivar13@ru.is>
2022-11-28 14:17:50 +0000

    Euler

--- Verkefni 2/Euler.py
+++ Verkefni 2/Euler.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+g = 9.81
+L =2
 f2 = lambda t, yinner: np.power(t, 2) +2
-f = lambda t, yinner: t * yinner - np.power(t, 3)
+#f = lambda t, yinner: t * yinner - np.power(t, 3)
+
+
+f = lambda yinner: np.array(yinner, (-g/L * np.sin(yinner)))
 
 def euler(y0,n,T):
     h = T/n
@@ -14,14 +19,14 @@ def euler(y0,n,T):
     for i in range(1, n+1):
         t = t+h
         xaxis.append(t)
-        yaxis.append(yaxis[i-1] + f(t, yaxis[i-1]) * h)
-
+        yaxis.append(yaxis[i-1] + f(yaxis[i-1]) * h)
+    print(xaxis)
+    print(yaxis)
     plt.plot(xaxis, yaxis)
 #    np.linspace(0,5)
 
 
-euler(2,5,3)
-euler(2,20,3)
-euler(2,200,3)
-fall(2,200,3)
+#euler(0,5,3)
+euler(0.1,20,3)
+#euler(0,500,3)
 plt.show()
