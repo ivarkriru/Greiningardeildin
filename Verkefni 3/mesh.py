ef796c700eb260a3eec6c18a33bdbf489049053f
Arnþór G <arnthorgisla@gmail.com>
2022-12-07 17:59:53 +0000

    vonandi búinn að laga Lmin og Lmax

--- Verkefni 3/mesh.py
+++ Verkefni 3/mesh.py
@@ -21,14 +21,13 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
         if (Lp[1] - Lp[0]) > y_max:
             raise ValueError("Lp má ekki vera stærra en y_max")
 
-        Lmin = int(Lp[0]/h)
-        Lmax = int(Lp[1]/h)
+        Lmin = int(Lp[0]/k)
+        Lmax = int(np.floor((Lp[1]/k)))
     else:
         if Lp > y_max:
             raise ValueError("Lp má ekki vera stærra en y_max")
         # hérna er best ef h gengur upp í Lp en þarf að testa
-        print(Lp/h)
-        padding = m - np.round(Lp/h)  # n[i] - Lp[cm]/h[cm/i]
+        padding = n - np.round(Lp/k)  # n[i] - Lp[cm]/h[cm/i]
         padding /= 2
         Lmin = int(padding)
         Lmax = int(n - padding)
@@ -89,7 +88,7 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
         A[t][t - 2 * m] = -1 / (2 * k)
 
     #  POWER
-    for j in range(Lmin, Lmax):
+    for j in range(Lmin, Lmax+1):
         i = 0
         t = i + (j) * (m)
         b[t] = -P / (L_ * delta * K)
@@ -113,9 +112,9 @@ def spurning4():
     A, b = bua_til_fylki(0, Lx, 0, Ly, n, m, Lp, P, H, K, delta, L)
 
 if __name__ == '__main__':
-    n, m = 10, 10
-    Lx, Ly = 2, 2
-    Lp = 1
+    n, m = 101, 11
+    Lx, Ly = 4, 4
+    Lp = (1,4)
     L = 2
     delta = 0.1
     H = 0.005
