f4dfc97c436757462a7ddc68edd974462719b945
Arnþór G <arnthorgisla@gmail.com>
2022-12-07 17:39:12 +0000

    bæta við Lp til að fá breytilega lengd á hitasvæði

--- Verkefni 3/mesh.py
+++ Verkefni 3/mesh.py
@@ -17,6 +17,21 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
     A = np.zeros((m * n, m * n))
     b = np.zeros((m * n, 1))
 
+    if type(Lp) is tuple:
+        if (Lp[1] - Lp[0]) > y_max:
+            raise ValueError("Lp má ekki vera stærra en y_max")
+
+        Lmin = int(Lp[0]/h)
+        Lmax = int(Lp[1]/h)
+    else:
+        if Lp > y_max:
+            raise ValueError("Lp má ekki vera stærra en y_max")
+        # hérna er best ef h gengur upp í Lp en þarf að testa
+        print(Lp/h)
+        padding = m - np.round(Lp/h)  # n[i] - Lp[cm]/h[cm/i]
+        padding /= 2
+        Lmin = int(padding)
+        Lmax = int(n - padding)
     # innra
 
     for i in range(1, m-1):
@@ -28,13 +43,28 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
             A[t][t + m] = 1 / k ** 2
             A[t][t - m] = 1 / k ** 2
 
-    # vinstri
-    for j in range(0, n):
+    # vinstri POWER
+    for j in range(Lmin, Lmax):
         i = 0
         t = i + (j) * (m)
         A[t][t] = -3 / (2 * h)
         A[t][t + 1] = 2 / h
         A[t][t + 2] = -1 / (2 * h)
+
+    # vinstri no POWER
+    for j in range(0, Lmin):
+        i = 0
+        t = i + (j) * (m)
+        A[t][t] = -3 / (2 * h) + H/K
+        A[t][t + 1] = 2 / h
+        A[t][t + 2] = -1 / (2 * h)
+    for j in range(Lmax, n):
+        i = 0
+        t = i + (j) * (m)
+        A[t][t] = -3 / (2 * h) + H/K
+        A[t][t + 1] = 2 / h
+        A[t][t + 2] = -1 / (2 * h)
+
     # hægri
     for j in range(0, n):
         i = m-1
@@ -59,7 +89,7 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
         A[t][t - 2 * m] = -1 / (2 * k)
 
     #  POWER
-    for j in range(0, n):
+    for j in range(Lmin, Lmax):
         i = 0
         t = i + (j) * (m)
         b[t] = -P / (L_ * delta * K)
@@ -67,7 +97,6 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta, L_):
     return A, b
 
 def spurning4():
-    n, m = 10, 10
     Lx, Ly = 2, 2
     Lp = 2
     L = 2
@@ -84,9 +113,9 @@ def spurning4():
     A, b = bua_til_fylki(0, Lx, 0, Ly, n, m, Lp, P, H, K, delta, L)
 
 if __name__ == '__main__':
-    n, m = 20, 10
+    n, m = 10, 10
     Lx, Ly = 2, 2
-    Lp = 2
+    Lp = 1
     L = 2
     delta = 0.1
     H = 0.005
@@ -107,8 +136,6 @@ if __name__ == '__main__':
     #     for j in range(n*n):
     #         print(f"{A[i,j]:.02f}", end="\t")
     #     print()
-    for vigur in A:
-        print(np.count_nonzero(vigur))
     #print(b)
     v = np.linalg.solve(A, b) + umhverfishiti
     print("Fyrir "+ str(n) +" X "+ str(m)+ " fylki er tíminn: "  f"{time.time()-t0:.02f}s")
