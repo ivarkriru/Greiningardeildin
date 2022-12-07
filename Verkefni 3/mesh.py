619c1e0dbe44b50280b52065d6536bd72a2e0592
Arnþór G <arnthorgisla@gmail.com>
2022-12-07 16:15:40 +0000

    plottar 3d

--- Verkefni 3/mesh.py
+++ Verkefni 3/mesh.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 from F import F, F_test, F_str
@@ -152,7 +154,7 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta):
     for j in range(0, n):
         i = m-1
         t = i + (j) * (m)
-        A[t][t] = -3 / (2 * h) - H / K
+        A[t][t] = -3 / (2 * h) + H / K
         A[t][t - 1] = 2 / h
         A[t][t - 2] = -1 / (2 * h)
 
@@ -182,7 +184,7 @@ def bua_til_fylki(x_min, x_max, y_min, y_max, n, m, Lp, P, H, K, delta):
 
 
 if __name__ == '__main__':
-    n, m = 4, 4
+    n, m = 50, 50
     Lx, Ly = 2, 2
     Lp = 2
     L = 1
@@ -196,25 +198,31 @@ if __name__ == '__main__':
     # ef Lp er float þá er powerið miðjað á gridið að lengd Lp
     # A, b = pde(0, Lx, 0, Ly, n, m, Lp, P, H)
     print("wha")
+    t0 = time.time()
     A, b = bua_til_fylki(0, Lx, 0, Ly, n, m, Lp, P, H, K, delta)
 
     print("fylki:")
-    for i in range(m*m):
-        for j in range(n*n):
-            print(f"{A[i,j]:.02f}", end="\t")
-        print()
+    # for i in range(m*m):
+    #     for j in range(n*n):
+    #         print(f"{A[i,j]:.02f}", end="\t")
+    #     print()
     #print(b)
     v = np.linalg.solve(A, b) + umhverfishiti
+    print(f"{time.time()-t0:.02f}s")
     w = v.reshape((m, n))
     print(b)
     print("bla")
     #print(v)
     #print(v.reshape((n,m)))
     print("max: ", np.max(w))
-    for i in range(m):
-        for j in range(n):
-            print(f"{w[i,j]:.02f}", end="\t")
-        print()
-
-    plt.pcolormesh(w)
+    # for i in range(m):
+    #     for j in range(n):
+    #         print(f"{w[i,j]:.02f}", end="\t")
+    #     print()
+    hf = plt.figure()
+    ha = hf.add_subplot(111, projection='3d')
+    from mpl_toolkits.mplot3d import Axes3D
+    X, Y = np.meshgrid([i for i in range(n)], [i for i in range(m)])  # `plot_surface` expects `x` and `y` data to be 2D
+    ha.plot_surface(X, Y, w)
+    #plt.pcolormesh(w)
     plt.show()
